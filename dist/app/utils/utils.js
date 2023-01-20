6808d0d73471a60c8ea0c522b775ebb8e499f712
Johnny <johnnyarza@gmail.com>
2023-01-19 19:34:59 -0800

    changes to test

--- dist/app/utils/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});const util = {
-  userRoles: ['admin', 'office', 'seller', 'lab', 'common'],
-  productsGrantedAccess: ['admin', 'office'],
-};
-
-exports. default = util;
